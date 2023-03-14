78aa333a4b70bbdd030ccd32b26805d7c17c779d
Nick Sandel <nsandel@rm.com>
2023-03-14 14:28:41 +0000

    Added in miniblog models and tests and setup for using pytest

--- miniblog/blog/models.py
+++ miniblog/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Blogger(models.Model):
+    """ Model representing a blog author. """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=1000, help_text='Enter a brief description of the blogger')
+
+    def __str__(self):
+        """ String for representing the Model object. """
+        return self.user.username
+
+class BlogPost(models.Model):
+    """ Model representing a blog post. """
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Blogger', on_delete=models.CASCADE)
+    content = models.TextField(max_length=5000, help_text='Enter the blog post content')
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """ String for representing the Model object. """
+        return self.title
+
+class BlogComment(models.Model):
+    """ Model representing a blog comment. """
+    post = models.ForeignKey('BlogPost', on_delete=models.CASCADE)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=1000, help_text='Enter the blog comment content')
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """ String for representing the Model object. """
+        return self.comment
---
+++ miniblog/blog/tests/test_models.py
@@ -0,0 +1,123 @@
+from django.test import TestCase
+from blog.models import Blogger, BlogPost, BlogComment
+from django.contrib.auth.models import User
+
+
+class BloggerModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        bob = User.objects.create_user(username="Bob", password="12345")
+        Blogger.objects.create(user=bob, bio="This is a bio")
+
+    def test_user_label(self):
+        blogger = Blogger.objects.get(id=1)
+        field_label = blogger._meta.get_field("user").verbose_name
+        self.assertEqual(field_label, "user")
+
+    def test_bio_label(self):
+        blogger = Blogger.objects.get(id=1)
+        field_label = blogger._meta.get_field("bio").verbose_name
+        self.assertEqual(field_label, "bio")
+
+    def test_bio_max_length(self):
+        blogger = Blogger.objects.get(id=1)
+        max_length = blogger._meta.get_field("bio").max_length
+        self.assertEqual(max_length, 1000)
+
+    def test_object_name_is_user(self):
+        blogger = Blogger.objects.get(id=1)
+        expected_object_name = f"{blogger.user}"
+        self.assertEqual(expected_object_name, str(blogger))
+
+    def test_string_representation(self):
+        blogger = Blogger.objects.get(id=1)
+        self.assertEqual(str(blogger), blogger.user.username)
+
+class BlogPostTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        bobUser = User.objects.create_user(username="Bob", password="12345")
+        bobBlogger = Blogger.objects.create(user=bobUser, bio="This is a bio")
+        BlogPost.objects.create(title="This is a title", author=bobBlogger, content="This is a content")
+
+    def test_title_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field("title").verbose_name
+        self.assertEqual(field_label, "title")
+
+    def test_author_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field("author").verbose_name
+        self.assertEqual(field_label, "author")
+
+    def test_content_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field("content").verbose_name
+        self.assertEqual(field_label, "content")
+
+    def test_date_posted_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field("date_posted").verbose_name
+        self.assertEqual(field_label, "date posted")
+
+    def test_date_updated_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field("date_updated").verbose_name
+        self.assertEqual(field_label, "date updated")
+
+    def test_title_max_length(self):
+        blogpost = BlogPost.objects.get(id=1)
+        max_length = blogpost._meta.get_field("title").max_length
+        self.assertEqual(max_length, 200)
+    
+    def test_content_max_length(self):
+        blogpost = BlogPost.objects.get(id=1)
+        max_length = blogpost._meta.get_field("content").max_length
+        self.assertEqual(max_length, 5000)
+
+    def test_object_name_is_title(self):
+        blogpost = BlogPost.objects.get(id=1)
+        expected_object_name = f"{blogpost.title}"
+        self.assertEqual(expected_object_name, str(blogpost))
+
+    def test_string_representation(self):
+        blogpost = BlogPost.objects.get(id=1)
+        self.assertEqual(str(blogpost), blogpost.title)
+
+class BlogCommentTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        bobUser = User.objects.create_user(username="Bob", password="12345")
+        bobBlogger = Blogger.objects.create(user=bobUser, bio="This is a bio")
+        blogPost = BlogPost.objects.create(title="This is a title", author=bobBlogger, content="This is a content")
+        sarahUser = User.objects.create_user(username="Sarah", password="12345")
+        BlogComment.objects.create(post=blogPost, commenter=bobUser, comment="This is a comment from Bob")
+        BlogComment.objects.create(post=blogPost, commenter=sarahUser, comment="This is a comment from Sarah")
+
+    def test_post_label(self):
+        blogcomment = BlogComment.objects.get(id=1)
+        field_label = blogcomment._meta.get_field("post").verbose_name
+        self.assertEqual(field_label, "post")
+        
+    def test_commenter_label(self):
+        blogcomment = BlogComment.objects.get(id=1)
+        field_label = blogcomment._meta.get_field("commenter").verbose_name
+        self.assertEqual(field_label, "commenter")
+
+    def test_comment_label(self):
+        blogcomment = BlogComment.objects.get(id=1)
+        field_label = blogcomment._meta.get_field("comment").verbose_name
+        self.assertEqual(field_label, "comment")
+
+    def test_date_posted_label(self):
+        blogcomment = BlogComment.objects.get(id=1)
+        field_label = blogcomment._meta.get_field("date_posted").verbose_name
+        self.assertEqual(field_label, "date posted")
+
+    def test_comment_max_length(self):
+        blogcomment = BlogComment.objects.get(id=1)
+        max_length = blogcomment._meta.get_field("comment").max_length
+        self.assertEqual(max_length, 1000)
